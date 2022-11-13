4f2702ceba4a4ab56ad12fdb2493bebf316daaf3
LenaBirrr <lenabir68@gmail.com>
2022-11-13 14:13:49 +0300

    Добавьте файлы проекта.

---
+++ SOA_6/PlagueMedication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOA_6
+{
+    public class PlagueMedication
+    {
+        public string Cure()
+        {
+            return "intravenous chloramphenicol with streptomycin";
+        }
+    }
+}
---
+++ SOA_6/SickPerson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOA_6
+{
+    public class SickPerson
+    {
+        public string Virus { get; set; }
+        public string Name { get; set; }
+
+
+        public void Recover(PlagueMedication medication)
+        {
+            Console.WriteLine(Name + " recovered from " + Virus + " with the help of " + medication.Cure());
+        }
+    }
+}
