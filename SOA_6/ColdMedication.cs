fba72ae07685513b6d34c35adfed18f0df6cb301
LenaBirrr <lenabir68@gmail.com>
2022-11-18 14:36:32 +0300

    Added class

---
+++ SOA_6/ColdMedication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOA_6
+{
+    public class ColdMedication:PlagueMedication
+    {
+
+    }
+}
