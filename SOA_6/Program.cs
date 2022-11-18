925c88276473798446b4a86bec7048e148f5d35f
LenaBirrr <lenabir68@gmail.com>
2022-11-18 13:22:47 +0300

    Example

--- SOA_6/Program.cs
+++ SOA_6/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SOA_6;
+
+SickPerson sick = new SickPerson();
+sick.Name = "Jack";
+sick.Virus = "Plague";
+
+IMedication medication = new PlagueMedication();
+
+sick.Recover(medication);
+
 Console.WriteLine("Hello, World!");
